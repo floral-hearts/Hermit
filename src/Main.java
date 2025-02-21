66acf534520f0fad859fbed3dbdb90a84406519e
floral-hearts <hermit183@outlook.com>
2025-02-21 22:46:05 +0900

    Create Main.java
    create mc frame
    name: Mountain Climbing

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String args[]) {
+        javax.swing.JFrame mcFrame = javax.swing.JFrame();
+        mcFrame.setTitle("Mountain Climbing");
+        mcFrame.setVisible(true);
+        mcFrame.setSize(200, 100);
+        mcFrame.setLocation(100, 100);
+    }
+}
