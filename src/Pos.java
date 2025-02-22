4a85aa606d0b288b0e0d33a03ef0ff1fff45227d
floral-hearts <hermit183@outlook.com>
2025-02-22 12:44:55 +0900

    Create Pos.java

---
+++ src/Pos.java
@@ -0,0 +1,15 @@
+public class Pos {
+    public long x;
+    public long y;
+    public long z;
+    public Pos() {
+        x = 0;
+        y = 0;
+        x = 0;
+    }
+    public Pos(int x, int y, iny z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+}
