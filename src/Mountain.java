70aa9f8745a63c0c55c3f829fc9d4cc59dc87b88
floral-hearts <hermit183@outlook.com>
2025-02-24 12:33:54 +0900

    Update Mountain.java

--- src/Mountain.java
+++ src/Mountain.java
@@ -6,11 +6,11 @@ public class Mountain {
     public static Pos zMax;
     public static Pos zMin;
     public static void set() {
-        xMax = Pos(0, 0, 0);
-        xMin = Pos(0, 0, 0);
-        yMax = Pos(0, 0, 0);
-        yMin = Pos(0, 0, 0);
-        zMax = Pos(0, 0, 0);
-        zMin = Pos(0, 0, 0);
+        xMax = Pos(9223372036854775807l, 0, 0);
+        xMin = Pos(-9223372036854775808l, 0, 0);
+        yMax = Pos(0, 9223372036854775807l, 0);
+        yMin = Pos(0, -9223372036854775808l, 0);
+        zMax = Pos();
+        zMin = Pos();
     }
 }
