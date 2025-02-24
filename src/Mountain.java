c0a999bb56902ad38a84759134fe69f7d900ccbc
floral-hearts <hermit183@outlook.com>
2025-02-24 12:09:39 +0900

    Create Mountain.java

---
+++ src/Mountain.java
@@ -0,0 +1,16 @@
+public class Mountain {
+    public static Pos xMax;
+    public static Pos xMin;
+    public static Pos yMax;
+    public static Pos yMin;
+    public static Pos zMax;
+    public static Pos zMin;
+    public static void set() {
+        xMax = Pos(0, 0, 0);
+        xMin = Pos(0, 0, 0);
+        yMax = Pos(0, 0, 0);
+        yMin = Pos(0, 0, 0);
+        zMax = Pos(0, 0, 0);
+        zMin = Pos(0, 0, 0);
+    }
+}
